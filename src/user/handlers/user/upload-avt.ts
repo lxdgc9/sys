df208998b2cdfa8ca40a2b78b5dcf1a5aa1ae3d7
DANH PHI LONG <longsf9@gmail.com>
2023-07-03 11:00:28 +0700

    pub update user

--- src/user/handlers/user/upload-avt.ts
+++ src/user/handlers/user/upload-avt.ts
@@ -2,6 +2,8 @@ import fs from "fs";
 import { RequestHandler } from "express";
 import { BadReqErr, UnauthorizedErr } from "@lxdgc9/pkg/dist/err";
 import { User } from "../../models/user";
+import { UpdateUserPublisher } from "../../events/publisher/user/update";
+import nats from "../../nats";
 
 const uploadAvt: RequestHandler = async (req, res, next) => {
   try {
@@ -29,6 +31,31 @@ const uploadAvt: RequestHandler = async (req, res, next) => {
     });
 
     res.sendStatus(200);
+
+    const _user = await User.findById(user._id).populate<{
+      role: {
+        name: string;
+      };
+    }>([
+      {
+        path: "role",
+        populate: {
+          path: "rules",
+          select: "-catalog",
+        },
+      },
+      {
+        path: "spec_rules",
+        select: "-catalog",
+      },
+    ]);
+    console.log("debug:", _user);
+    await new UpdateUserPublisher(nats.cli).publish({
+      id: _user!._id,
+      attrs: _user!.attrs,
+      is_active: _user!.is_active,
+      role: _user!.role.name,
+    });
   } catch (e) {
     next(e);
     if (req.file) {
